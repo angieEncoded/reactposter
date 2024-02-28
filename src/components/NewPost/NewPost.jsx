c514cc5dfffe183fbb46c64840f0c14ef4cd2850
angieEncoded <43992078+angieEncoded@users.noreply.github.com>
2024-02-28 05:49:52 -0600

    Move the post submit logic

--- src/components/NewPost/NewPost.jsx
+++ src/components/NewPost/NewPost.jsx
@@ -1,21 +1,41 @@
 import classes from "./NewPost.module.css"
+import {useState} from 'react';
 
+function NewPost({buttonCloseModal}){
 
+  const [textAreaState, settextAreaState] = useState('');
+  const [authorState, setAuthorState] = useState('');
 
+  // Keep track of changes in the textarea
+  const changeBodyHandler = async(event) => {
+      settextAreaState(event.target.value);
+  }
 
-function NewPost({textAreaDataEntered, authorNameEntered, buttonCloseModal}){
+  const setAuthorName = async(event) => {
+      setAuthorState(event.target.value)
+  }
+
+  const submitForm = async(event) => {
+    event.preventDefault();
+    const postData = {
+      body: textAreaState, // add from the state slices
+      author: authorState
+    }
+    console.log(postData)
+    buttonCloseModal(); // shut the modal
+  }
 
 return (
-<form className={classes.form}>
+<form className={classes.form} onSubmit={submitForm}>
       <p>
         <label htmlFor="body">Text</label>
                                               {/* Get the state from the other component */}
-        <textarea id="body" required rows={3} onChange={textAreaDataEntered}/>
+        <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
       </p>
 
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={authorNameEntered}/>
+        <input type="text" id="name" required onChange={setAuthorName}/>
       </p>
       <p className={classes.actions}>
         <button type="button" onClick={buttonCloseModal}>Cancel</button>
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -7,37 +7,20 @@ import Modal from "../Modal/Modal"
 
 function PostList({donePosting, modalIsVisibleState}){
 
-    const [textAreaState, settextAreaState] = useState('');
-    const [authorState, setAuthorState] = useState('');
-
-
-    // Keep track of changes in the textarea
-    const changeBodyHandler = async(event) => {
-        settextAreaState(event.target.value);
-    }
-
-    const setAuthorName = async(event) => {
-        setAuthorState(event.target.value)
-    }
-
     return (
         <>
             {modalIsVisibleState && 
             <Modal onClose={donePosting}>
                 {/* pass this function into the prop so we can use it in the other component */}
                 <NewPost 
-                    textAreaDataEntered={changeBodyHandler} 
-                    authorNameEntered={setAuthorName}
                     buttonCloseModal={donePosting}
-
                 />
             </Modal> }
 
 
-            <ul className={classes.posts}>    
-                <Post author={authorState} body={textAreaState}/>
-                <Post author="Shuttle" body="Shakey is an ass hole"/>
-                <Post author="Sasha" body="Both my sisters are idiots"/>
+            <ul className={classes.posts}>   
+
+
             </ul>
         </>
 
