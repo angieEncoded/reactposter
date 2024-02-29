38c021fbca122de91430795fcf38bdcf8a64dff9
angieEncoded <43992078+angieEncoded@users.noreply.github.com>
2024-02-29 07:09:55 -0600

    Get basic routing working

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import NewPost from './components/NewPost/NewPost'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+// all the routes
+const router = createBrowserRouter([
+  {path:'/', element: <App />}, // main route
+  {path:'/create-post', element: <NewPost/> }, 
+]);
+
 
 // Injection point - into the root div
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}>
+
+    </RouterProvider>
+    {/* <App /> */}
   </React.StrictMode>
 )
